b488da1c7de560764feb6207c073542c04f0a66b
Las <haho013039@gmail.com>
2021-04-03 23:49:11 +0900

    Dockerlize `weather` and deploy frontend and server (#14)
    `weather`프로젝트에 docker를 적용하고, 배포하는 경우 서버와 같이 배포되도록 하였습니다.
    이제는 리액트 페이지로 접근 가능해요! 그리고 api들은 api.atmop.dev로 이용 가능해요!

--- deploy.sh
+++ deploy.sh
@@ -6,12 +6,23 @@ docker rm $(docker ps -aq)
 echo "STOP ALL DOCKER CONTAINERS -> DONE"
 
 timestamp=`date +%Y%m%d%H%M`
-docker_image_name=atmo-server-"$timestamp"
 
-echo "docker iamge name is " + "$docker_image_name"
+echo "SERVER DEPLOY START"
 
-docker build -t "$docker_image_name" .
+server_docker_image_name=atmo-server-"$timestamp"
+echo "docker iamge name is " + "$server_docker_image_name"
 
-docker run -d -p 8080:8080 "$docker_image_name"
+docker build -t "$server_docker_image_name" .
+docker run -d -p 8081:8080 "$server_docker_image_name"
+echo "SERVER DEPLOY DONE"
+
+echo "FRONTEND DEPLOY START"
+
+front_docker_image_name=atmo-front-"$timestamp"
+echo "docker iamge name is " + "$front_docker_image_name"
+
+docker build -t "$front_docker_image_name" ./frontend/weather/
+docker run -d -p 8080:3000 "$front_docker_image_name"
+echo "FRONTEND DEPLOY DONE"
 
 echo "DEPLOY END"
