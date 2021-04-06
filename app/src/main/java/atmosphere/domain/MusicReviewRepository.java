f2f45c758f5b4aba05a319892892dbc7b6e153d4
Las <haho013039@gmail.com>
2021-04-06 19:07:04 +0900

    Repository를 만들다
    기존에는 Repository라는 개념 없이 Application Service에서 list로 관리하고 있었습니다.
    그러나 이런 도메인 객체를 저장하고 가져오는 것을 Repository로 책임을 위임하면 Application Service에서는 Application 비즈니스 로직에만 집중할 수 있습니다.
    따라서 Repository를 구현하고 의존성 주입을 통해서 Application Service가 사용하도록 하였습니다.

---
+++ app/src/main/java/atmosphere/domain/MusicReviewRepository.java
@@ -0,0 +1,9 @@
+package atmosphere.domain;
+
+import java.util.List;
+
+public interface MusicReviewRepository {
+    List<MusicReview> getAll();
+    void add(MusicReview musicReview);
+    void deleteAll();
+}
