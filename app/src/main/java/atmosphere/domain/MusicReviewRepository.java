cb9b228c612edf0819de01b85148c6d3e68ddd92
Las <haho013039@gmail.com>
2021-04-06 19:39:11 +0900

    Implement find specific review using id
    41764122f43d775724195b99f8b228d3628e2d3f 에서 구현한 아이디를 기반으로 하나의 Review를 찾는 유즈케이스를 개발하였습니다.

--- app/src/main/java/atmosphere/domain/MusicReviewRepository.java
+++ app/src/main/java/atmosphere/domain/MusicReviewRepository.java
@@ -1,10 +1,12 @@
 package atmosphere.domain;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface MusicReviewRepository {
     List<MusicReview> getAll();
     void add(MusicReview musicReview);
     void deleteAll();
     Long nextId();
+    Optional<MusicReview> findById(Long id);
 }
